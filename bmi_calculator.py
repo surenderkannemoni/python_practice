dab1e1518ca82835046896cba1e42e7f2b2f87c1
Surender <surender.kannemoni@gmail.com>
2022-12-27 15:55:10 +0530

    remove testing line

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -3,5 +3,4 @@ weight = float(input('Enter your weight: '))
 height = float(input('Enter your height: '))
 bmi = round(weight / height ** 2)
 print(bmi)
-# bmi calculator 
-#testing
+# bmi calculator
