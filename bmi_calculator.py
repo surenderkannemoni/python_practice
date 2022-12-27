274e58fe51572a74a55310b7312e8ddc516d8499
surenderkannemoni <91527617+surenderkannemoni@users.noreply.github.com>
2022-12-27 15:50:48 +0530

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -3,4 +3,5 @@ weight = float(input('Enter your weight: '))
 height = float(input('Enter your height: '))
 bmi = round(weight / height ** 2)
 print(bmi)
-# bmi calculator
+# bmi calculator 
+#testing
