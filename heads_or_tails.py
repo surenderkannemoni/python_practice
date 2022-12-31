b89251b2732bcc5c119a8bfa242ed2206510c5cb
surenderkannemoni <91527617+surenderkannemoni@users.noreply.github.com>
2022-12-31 16:22:01 +0530

    Staging (#20)
    * create program (#7)
    
    * Feature/life in weeks (#10) (#11)
    
    * create program
    
    * create program
    
    * Feature/tip calculator1 (#13)
    
    * initial commit
    
    * instructions
    
    * add instructions
    
    * initial commit
    
    * Feature 123 validator (#3)
    
    * add commit
    
    * Update bmi_calculator.py
    
    * remove testing line
    
    * create program
    
    * create program (#7) (#9)
    
    * create program
    
    * Staging (#12)
    
    * create program (#7)
    
    * Feature/life in weeks (#10) (#11)
    
    * create program
    
    * create program
    
    * initial commit
    
    * Revert "Feature/tip calculator1 (#13)" (#16)
    
    This reverts commit 4dc5f2e207f06f4cfb03b364e93e82b8abe0ba81.
    
    * Dev (#19)
    
    * Feature/life in weeks (#10)
    
    * create program
    
    * create program
    
    * Feature/tip calculator1 (#13)
    
    * initial commit
    
    * instructions
    
    * add instructions
    
    * initial commit
    
    * Feature 123 validator (#3)
    
    * add commit
    
    * Update bmi_calculator.py
    
    * remove testing line
    
    * create program
    
    * create program (#7) (#9)
    
    * create program
    
    * Staging (#12)
    
    * create program (#7)
    
    * Feature/life in weeks (#10) (#11)
    
    * create program
    
    * create program
    
    * initial commit
    
    * Heads or tails (#18)
    
    * create program
    
    * initital commit

---
+++ heads_or_tails.py
@@ -0,0 +1,14 @@
+import random
+
+print('Welcome to Heads or Tails')
+ran = random.randint(0, 1)
+
+if ran == 1:
+    print('Heads')
+elif ran == 0:
+    print('Tails')
+else:
+    print('Invalid INput')
+
+
+
